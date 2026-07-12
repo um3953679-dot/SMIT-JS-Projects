d2439d98a8f03b81f944f7d15b1eb0a61d1feca5
Muhammad Umer 2010 <um3953679@gmail.com>
2026-07-12 19:42:43 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-console.log('Hello, World!');
-console.log('Hello,My name is Muhammad Umer!');
-console.log('I am 15 years old!');
-alert('Hello, World!');
+
+
+
+
+
+// Task no 1
+alert("Hello World!")
+
+
+let name = "Muhammad Umer";
+console.log(name);
+
+let city = "Karachi";
+console.log(city);
+
+let age = 16;
+console.log(age);
+
+let course = "JavaScript";
+console.log(course);
+
+let country = "Pakistan";
+console.log(country);
+
+
+// task no 2 value change
+
+let language = "HTML";
+console.log(language);
+
+language = "JavaScript";
+console.log(language);
