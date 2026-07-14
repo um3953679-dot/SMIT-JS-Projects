e9517c836d8d53196ec9fe1f570db39ca16108c3
Muhammad Umer 2010 <um3953679@gmail.com>
2026-07-14 18:27:12 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
+// Task no 1 //
 
-
-
-
-
-// Task no 1
-alert("Hello World!")
-
-
-let name = "Muhammad Umer";
-console.log(name);
-
-let city = "Karachi";
-console.log(city);
+let fullname = "muhammadumer";
+console.log(fullname);
 
 let age = 16;
 console.log(age);
 
-let course = "JavaScript";
-console.log(course);
+let price = 2000;
+console.log(price);
 
-let country = "Pakistan";
-console.log(country);
+let marks = 100;
+console.log(marks);
+
+let hours = 24;
+console.log(hours);
+
+//Task no 2 //
+
+//Legal variable name //
+
+let studentName = "Umer";
+let _marks = 85;
+let $price = 2500;
 
 
-// task no 2 value change
+//illegal variable name //
 
-let language = "HTML";
-console.log(language);
+// let 1student = "Umer";   // Number se start nahi ho sakta
+// let my-name = "Ali";     // Hyphen (-) allow nahi hai
+// let var = 100;           // Reserved keyword hai
 
-language = "JavaScript";
-console.log(language);
+
+//Task no 3 Mathematic Exprssion //
+
+
+let num1 = 100;
+let num2= 20;
+console.log(num1 + num2); // addition
+console.log(num1 - num2); // subtraction
+console.log(num1 * num2); // multiplication
+console.log(num1 / num2); // division
+console.log(num1 % num2); // modulus
+
+
