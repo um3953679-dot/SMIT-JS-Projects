1665df83950b8ff01b2039315d159b0270e8deb8
Muhammad Umer 2010 <um3953679@gmail.com>
2026-07-18 23:30:22 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,60 +1,82 @@
-// Question 1 //
-
-let num = 10;
-
-num++;
-
-console.log(num);
-
-// Question 2 //
-
-let count = 20;
-
-count--;
-
-console.log(count);
-
-// Question 3 //
-
-let age = 18;
-
-++age;
-
-console.log(age);
-
-//Question 4 //
-
-let marks = 95;
-
---marks;
-
-console.log(marks);
-
-// Question 5 //
-
-let x = 5;
-
-console.log(x++);
-
-console.log(x);
-
-// Question 6 //
-
-console.log(10 + 5 * 2);
-
-// Question 7 //
-
-console.log((10 + 5) * 2);
-
-// Bonus Question //
-
-let num1 = 25;
-let num2 = 5;
-
-console.log("Addition:", num1 + num2);
-console.log("Subtraction:", num1 - num2);
-console.log("Multiplication:", num1 * num2);
-console.log("Division:", num1 / num2);
-console.log("Modulus:", num1 % num2);
 
 
+// ======================
+// Question 1
+// ======================
+
+let firstName = "Muhammad";
+let lastName = "Umer";
+
+console.log(firstName);
+console.log(firstName + " " + lastName);
+
+
+// ======================
+// Question 2
+// ======================
+
+let city = "Karachi";
+
+console.log("I live in " + city);
+
+
+// ======================
+// Question 3
+// ======================
+
+let userName = prompt("Enter your name");
+
+console.log("Welcome " + userName);
+
+
+// ======================
+// Question 4
+// ======================
+
+let first = prompt("Enter your First Name");
+let last = prompt("Enter your Last Name");
+
+console.log(first + " " + last);
+
+
+// ======================
+// Question 5
+// ======================
+
+let favoriteColor = prompt("Enter your Favourite Color");
+
+console.log("Your favourite color is " + favoriteColor + ".");
+
+
+// ======================
+// Question 6
+// ======================
+
+let country = "Pakistan";
+let myCity = "Karachi";
+
+console.log("I live in " + myCity + ", " + country + ".");
+
+
+// ======================
+// Question 7
+// ======================
+
+let name = prompt("Enter your Name");
+let cityName = prompt("Enter your City");
+let countryName = prompt("Enter your Country");
+
+console.log("Hello " + name + "!");
+console.log("Welcome to " + cityName + ", " + countryName + ".");
+
+
+// ======================
+// Question 8
+// ======================
+
+let first_Name = "Muhammad";
+let last_Name = "Umer";
+let course = "MERN Stack Development in SMIT";
+
+console.log("Hello " + first_Name + " " + last_Name + "!");
+console.log("Welcome to the " + course + " Course.");
